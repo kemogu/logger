bcc3a89e7d0106ba5a909ea0d2952d83906eb42e
kemogu <kemaloguzhanyeyit@gmail.com>
2025-04-14 22:37:01 +0300

    logLevel added.

---
+++ LogLevel.h
@@ -0,0 +1,14 @@
+/**
+ * @enum LogLevel
+ * 
+ * @brief Defines the available log levels.
+ * 
+ * @date 14.04.2025
+ * @author Kemal Oğuzhan Yeyit
+ */
+enum class LogLevel {
+    DEBUG = 0,
+    INFO,
+    WARN,
+    ERROR
+};
